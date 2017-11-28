a9bc32337434dcd58d99f7183e209909f4829e6e
sakib-shahriyar <shaheen840@gmail.com>
2017-11-28 17:25:24 +0600

    New Features

--- src/potatoBrowser/FXMLDocumentController.java
+++ src/potatoBrowser/FXMLDocumentController.java
@@ -12,10 +12,12 @@ import java.io.PrintWriter;
 import java.lang.Thread.State;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Vector;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
 import javafx.event.ActionEvent;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -53,17 +55,39 @@ public class FXMLDocumentController implements Initializable {
     private Button btnGag;
     @FXML
     private Button btnYoutube;
+    @FXML
+    private Button buttonBack;
+    @FXML
+    private Button buttonFront;
     
-    
+    private static Vector<String> vec = new Vector();
+    private int track;
+    private int flag;
+    private boolean isClicked = false;
     @FXML
     private void handleButtonAction(ActionEvent event) throws FileNotFoundException, IOException {
         pi.progressProperty().bind(web.getLoadWorker().progressProperty());
+        String str = textFieldUrl.getText();
+        if (isClicked == true || vec.lastElement().equals(str)) {
+           
+        }else vec.addElement(str);
+        
         web.load(textFieldUrl.getText().startsWith("http://") || textFieldUrl.getText().startsWith("https://") ? textFieldUrl.getText() : "http://" + textFieldUrl.getText());
+        System.out.println(vec.size());
+        for (int i = 0; i < vec.size(); i++) {
+            System.out.println(vec.elementAt(i));
+        }
+
+        if(vec.size()>1){
+           buttonBack.setDisable(false);
+       }
     }  
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        flag = 0;
+        isClicked = false;
         web = webView.getEngine();
         web.setJavaScriptEnabled(true);
         web.locationProperty().addListener(new ChangeListener<String>() {
@@ -84,52 +108,76 @@ public class FXMLDocumentController implements Initializable {
                    // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
         });
+            
+       buttonFront.setDisable(true);
+       buttonBack.setDisable(true);
        pi.progressProperty().bind(web.getLoadWorker().progressProperty());
        web.load("https://www.google.com");
+       vec.addElement("https://www.google.com");
     }
 
     @FXML
     private void redditButtonAction(ActionEvent event) throws IOException {
-        pi.progressProperty().bind(web.getLoadWorker().progressProperty());
-        web.load("http://www.reddit.com");
+        textFieldUrl.setText("http://www.reddit.com");
+        buttonGo.fire();
     }
 
     @FXML
     private void googleButtonAction(ActionEvent event) throws IOException {
-        pi.progressProperty().bind(web.getLoadWorker().progressProperty());
-        web.load("http://www.google.com");
+        textFieldUrl.setText("http://www.google.com");
+        buttonGo.fire();
     }
 
     @FXML
     private void gmailButtonAction(ActionEvent event) throws IOException {
-        pi.progressProperty().bind(web.getLoadWorker().progressProperty());
-        web.load("http://www.gmail.com");
+        textFieldUrl.setText("http://www.gmail.com");
+        buttonGo.fire();
     }
 
     @FXML
     private void ymailButtonAction(ActionEvent event) throws IOException {
-        pi.progressProperty().bind(web.getLoadWorker().progressProperty());
-        web.load("http://www.ymail.com");
+        textFieldUrl.setText("http://www.ymail.com");
+        buttonGo.fire();
     }
 
     @FXML
     private void twitterButtonAction(ActionEvent event) {
-        pi.progressProperty().bind(web.getLoadWorker().progressProperty());
-        web.load("http://www.twitter.com");
-    
+        textFieldUrl.setText("http://www.twitter.com");
+        buttonGo.fire();
     }
 
     @FXML
     private void gagButtonAction(ActionEvent event) {
-        pi.progressProperty().bind(web.getLoadWorker().progressProperty());
-        web.load("http://www.9gag.com");
+        textFieldUrl.setText("http://www.9gag.com");
+        buttonGo.fire();
     }
 
     @FXML
     private void tubeButtonAction(ActionEvent event) {
-        pi.progressProperty().bind(web.getLoadWorker().progressProperty());
-        web.load("http://www.youtube.com");
-
+        textFieldUrl.setText("http://www.youtube.com");
+        buttonGo.fire();
     }
 
+    @FXML
+    private void backButtonAction(ActionEvent event) {
+        isClicked = true;
+        buttonFront.setDisable(false);
+        flag++;
+        textFieldUrl.setText(vec.elementAt(vec.size()-flag-1));
+        buttonGo.fire();
+        track=flag;
+        if(vec.elementAt(vec.size()-flag-1)== vec.firstElement()){
+            buttonBack.setDisable(true);
+        }
+    }
+
+    @FXML
+    private void frontButtonAction(ActionEvent event) {
+        pi.progressProperty().bind(web.getLoadWorker().progressProperty());
+        textFieldUrl.setText(vec.elementAt(vec.size()-flag));
+        if(vec.elementAt(vec.size()-flag)== vec.lastElement()){
+            buttonFront.setDisable(true);
+        }
+        track++;
+    } 
 }
